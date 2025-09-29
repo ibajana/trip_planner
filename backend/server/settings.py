6c13196d672fed00f9a75d52c5f97ec6d1d47375
ibajana <ibajana@conpronto.com>
2025-09-29 17:39:17 -0500

    Configurar Procfile para Render

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kqwhz5hj4hx+ua4av+*s575f@qn2=^&$@#if%a3&&n7-&i6h!t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
